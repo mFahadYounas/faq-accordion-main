4604a482cdbaf3afe5236e378f7c8d81b099639e
Fahad <fahadyounas97@outlook.com>
2024-04-07 16:08:52 +0500

    Moved allQna and makeQna function into separate files

---
+++ src/MakeQna.ts
@@ -0,0 +1,27 @@
+export const makeQna = (qText: string, aText: string) => {
+  const accordionBody = document.querySelector(".accordion-body");
+  const qna = document.createElement("li");
+  qna.classList.add("qna");
+  // question section
+  const question = document.createElement("section");
+  question.classList.add("question");
+  const qButton = document.createElement("button");
+  qButton.textContent = qText;
+  const expandSpan = document.createElement("span");
+  const expandImg = document.createElement("img");
+  expandImg.src = "./src/assets/images/icon-plus.svg";
+  expandImg.alt = "plus icon";
+  expandSpan.appendChild(expandImg);
+  qButton.appendChild(expandSpan);
+  question.appendChild(qButton);
+  qna.appendChild(question);
+  accordionBody?.appendChild(qna);
+  // answer section
+  const answer = document.createElement("section");
+  answer.classList.add("answer");
+  answer.classList.add("hidden");
+  const answerParagraph = document.createElement("p");
+  answerParagraph.innerHTML = aText;
+  answer.appendChild(answerParagraph);
+  qna.appendChild(answer);
+};
