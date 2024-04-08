0108382b368349fe6a36d83f6bfb4e45c8c2af11
Fahad <fahadyounas97@outlook.com>
2024-04-08 21:28:39 +0500

    Added transitions to collapsing and expanding of the answer panels

--- src/App.ts
+++ src/App.ts
@@ -7,7 +7,7 @@ allQna.forEach((qna) => {
 
 const questionButton = document.querySelectorAll(".question > button");
 
-const answer = document.querySelectorAll(".answer");
+const answer = document.querySelectorAll(".answer") as NodeListOf<HTMLElement>;
 
 const plusMinusImg = document.querySelectorAll(
   ".question > button > span > img"
@@ -18,11 +18,11 @@ let answerState: boolean[] = new Array(questionButton.length).fill(false);
 questionButton.forEach((button, i) => {
   button.addEventListener("click", () => {
     if (!answerState[i]) {
-      answer[i].classList.remove("hidden");
+      answer[i].style.maxHeight = "400px";
       plusMinusImg[i].setAttribute("src", "./src/assets/images/icon-minus.svg");
       answerState[i] = true;
     } else {
-      answer[i].classList.add("hidden");
+      answer[i].style.maxHeight = "0";
       answerState[i] = false;
       plusMinusImg[i].setAttribute("src", "./src/assets/images/icon-plus.svg");
     }
